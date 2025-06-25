98d12c20f0869f0322af6bcdfb59158db8924d50
sharkipelago <gchozick@gmail.com>
2025-06-25 15:16:43 -0400

    added bootstrap-flask

--- bugbox/__init__.py
+++ bugbox/__init__.py
@@ -1,13 +1,13 @@
 import os
-
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'bugbox.sqlite'),
     )
 
     if test_config is None:
@@ -23,10 +23,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    app.config["BOOTSTRAP_BOOTSWATCH_THEME"] = 'quartz' #https://bootswatch.com/
+    Bootstrap5(app) # adds boostrap object https://bootstrap-flask.readthedocs.io/en/stable/basic/
     
     from . import db
     db.init_app(app)
