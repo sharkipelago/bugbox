a5a8e200384d815d40bbecee0ec0f52561b51028
sharkipelago <gchozick@gmail.com>
2025-07-24 13:45:36 -0400

    added details template

--- bugbox/db.py
+++ bugbox/db.py
@@ -32,21 +32,6 @@ DEFAULT_ISSUES = [
 ]
 
 
-def issue_team_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        team_ids = get_issue_teams(kwargs.get('issue_id'))
-        assert isinstance(team_ids, int) or isinstance(team_ids, set)
-        # Allows to pass in a single team_id or multiple ids as a set
-        if not isinstance(team_ids, set):
-            team_ids = set(team_ids) 
-        if g.user['admin_level'] != 2 and g.user["team_id"] not in team_ids:
-            return redirect(url_for('admin.denied'))
-
-        return view(**kwargs)
-
-    return wrapped_view
-
 def dml_operation(db_func):
     @functools.wraps(db_func)
     def wrapper(*args, **kwargs):
--- bugbox/issue.py
+++ bugbox/issue.py
@@ -1,12 +1,48 @@
-from collections import defaultdict
-
+import functools
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
-from bugbox.auth import login_required, team_lead_required, issue_team_required
+from bugbox.auth import login_required, team_lead_required
 from bugbox.db import get_db, get_user, get_users, get_issue_teams, get_assignees, get_team_names, create_issue, insert_assignment, update_issue_progress
 
 bp = Blueprint('issue', __name__)
+
+# ==View Wrappers==
+# Can Update, Delete, Edit Assignees, Mark as Reviewed and Close an issue in addition to contribute_perms
+def modify_perms_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        print("ID", kwargs.get('issue_id'))
+        team_ids = get_issue_teams(kwargs.get('issue_id'))
+        print("PRINTING IDS")
+        print(team_ids)
+        assert isinstance(team_ids, set)
+
+        print("HEEERE")
+        if (g.user['admin_level'] == 2 or 
+            g.user['admin_level'] == 1 and g.user['team_id'] in team_ids):
+            return view(**kwargs)
+        # TODO custom modify_perms denied page
+        return redirect(url_for('admin.denied'))
+
+    return wrapped_view
+# Can add updates on comment chain and submit issue
+def contribute_perms_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        team_ids = get_issue_teams(kwargs.get('issue_id'))
+        assert isinstance(team_ids, set)
+        assignee_ids = [a['id'] for a in get_assignees(kwargs.get('issue_id'))]
+
+        if (g.user['admin_level'] == 2 or 
+            g.user['admin_level'] == 1 and g.user['team_id'] in team_ids or
+            g.user['admin_level'] < 1 and g.user['id'] in assignee_ids):
+            return view(**kwargs)
+        # TODO custom modify_perms denied page
+        return redirect(url_for('admin.denied'))
+
+    return wrapped_view
+
 
 def get_assignments():
     return get_db().execute(
@@ -53,7 +89,7 @@ def create():
 
     return render_template('issue/create.html')
 
-def get_issue(id, check_author=True):
+def get_issue(id):
     issue = get_db().execute(
         'SELECT *'
         ' FROM issue i JOIN user u ON i.author_id = u.id'
@@ -64,52 +100,55 @@ def get_issue(id, check_author=True):
     if issue is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    admin_access = issue['team_id'] == g.user['team_id'] and g.user['admin_level'] > 0
-    if check_author and issue['author_id'] != g.user['id'] and not admin_access:
-        abort(403)
+    # admin_access = issue['team_id'] == g.user['team_id'] and g.user['admin_level'] > 0
+    # if check_author and issue['author_id'] != g.user['id'] and not admin_access:
+    #     abort(403)
 
     return issue
 
-@bp.route('/<int:issue_id>/update', methods=('GET', 'POST'))
+@bp.route('/<int:issue_id>/details')
 @login_required
-def update(issue_id):
+def details(issue_id):
     issue = get_issue(issue_id)
+    return render_template('issue/details.html', issue=issue, assignees=get_assignees(issue_id), users=get_users())
 
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE issue SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, issue_id)
-            )
-            db.commit()
-            return redirect(url_for('issue.index'))
-    return render_template('issue/update.html', issue=issue, assignees=get_assignees(issue_id), users=get_users())
-
-@bp.route('/<int:id>/delete', methods=('POST',))
+@bp.route('/<int:issue_id>/update', methods=('POST',))
 @login_required
-@issue_team_required
-def delete(id):
-    get_issue(id)
+@modify_perms_required
+def update(issue_id):
+    print("WE even updating?")
+    title = request.form['title']
+    body = request.form['body']
+    error = None
+
+    if not title:
+        error = 'Title is required.'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'UPDATE issue SET title = ?, body = ?'
+            ' WHERE id = ?',
+            (title, body, issue_id)
+        )
+        db.commit()
+    return redirect(url_for('issue.details', issue_id=issue_id))
+
+@bp.route('/<int:issue_id>/delete', methods=('POST',))
+@login_required
+@modify_perms_required
+def delete(issue_id):
+    get_issue(issue_id)
     db = get_db()
-    db.execute('DELETE FROM issue WHERE id = ?', (id,))
+    db.execute('DELETE FROM issue WHERE id = ?', (issue_id,))
     db.commit()
     return redirect(url_for('issue.index'))
 
 @bp.route('/<int:issue_id>/<int:user_id>/add-assignee', methods=('GET',))
 @login_required
-@issue_team_required
-@team_lead_required
+@modify_perms_required
 def add_assignee(issue_id, user_id):
     assert g.user['team_id'] in get_issue_teams(issue_id)
     assert get_user(user_id)['team_id'] in get_issue_teams(issue_id)
@@ -118,12 +157,11 @@ def add_assignee(issue_id, user_id):
     cursor = db.cursor()
     insert_assignment(cursor, issue_id, user_id)
     db.commit()
-    return redirect(url_for('issue.update', issue_id=issue_id))
+    return redirect(url_for('issue.details', issue_id=issue_id))
 
 @bp.route('/<int:issue_id>/<int:assignee_id>/remove-assignee', methods=('GET',))
 @login_required
-@issue_team_required
-@team_lead_required
+@modify_perms_required
 def remove_assignee(issue_id, assignee_id):
     assert g.user['team_id'] in get_issue_teams(issue_id)
     assert get_user(assignee_id)['team_id'] in get_issue_teams(issue_id)
@@ -132,7 +170,7 @@ def remove_assignee(issue_id, assignee_id):
     cursor = db.cursor()
     delete_assignment(cursor, issue_id, assignee_id)
     db.commit()
-    return redirect(url_for('issue.update', issue_id=issue_id))
+    return redirect(url_for('issue.details', issue_id=issue_id))
 
 @login_required
 # TODO assignee required
@@ -148,4 +186,3 @@ def submit_issue(issue_id, submitter_id):
 def close_issue(issue_id, closer_id):
     update_issue_progress(issue_id, 2)
     return redirect(url_for('issue.index'))
-
